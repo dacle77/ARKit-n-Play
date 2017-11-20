10515131532a70239e22fecc4fd50c8a36f2f001
dacle77 <cleland.austin@dtn.com>
2017-11-20 15:42:54 -0800

    added new testProject

---
+++ testEmpty/testEmpty/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  testEmpty
+//
+//  Created by cleland on 11/20/17.
+//  Copyright © 2017 Cleland Austin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        // Added some words to comment
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
